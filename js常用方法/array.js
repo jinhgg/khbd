8919275c774ec769efb890bbbe0e18f2a82b4c6a
jinhgg <970402782@qq.com>
2021-05-20 14:05:37 +0800

    js过滤

--- js常用方法/array.js
+++ js常用方法/array.js
@@ -1,3 +1,6 @@
-const arr = [1,2,3,4]
+const arr = [1, 2, 3, 4]
 // 1.判断arr是否包含某个元素
 arr.includes(1)
+// 2.按条件过滤
+arr = [{ a: 11, b: 22, c: 33 }, { a: 11, b: 22, c: 33 }, { a: 22, b: 22, c: 33 }]
+new_arr = arr.filter(item => item.a !== 22) // 去除a为22的元素
