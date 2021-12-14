05341de469e1bcb92bb5fe7c668aa0777229032c
jinhgg <970402782@qq.com>
2021-12-14 10:44:17 +0800

    添加supervisord的root安装方法

---
+++ python/playground.py
@@ -0,0 +1,11 @@
+a = [1,3,4,5]
+print(a.index(3))
+print(a[1:])
+# def maxSubArray(nums):
+#     f = [nums[0]]
+#     for i in range(1, len(nums)):
+#         f.append(max((f[-1]+nums[i]), nums[i]))
+#         print(f)
+#     return max(f)
+
+# maxSubArray(a)
