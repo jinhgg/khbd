8ade18d38fdbd9ffa7bb27605a220a08157f9232
jinhgg <970402782@qq.com>
2022-12-13 21:40:18 +0800

    学了一大堆

--- java/TestDemo.java
+++ java/TestDemo.java
@@ -1,15 +1,22 @@
 import java.util.ArrayList;
-import java.util.StringJoiner;
-
-import 继承.Student;
 
 public class TestDemo {
-    public static void main(String[] args) {
-        Student s = new Student();
-        String str = s.label;
-
-
-        
-
+    // 这里用static是因为，初始化的是类属性，不是实例属性
+    static ArrayList<Student> list = new ArrayList<>();
+    // 这里用static是因为，静态代码块就是这样写的
+    static {
+        Student s1 = new Student();
+        Student s2 = new Student();
+        Student s3 = new Student();
+        list.add(s1);
+        list.add(s2);
+        list.add(s3);
     }
+    public static void main(String[] args) {
+        System.out.println(list);
+    }
+}
+
+class Student {
+    public Student() {}
 }
